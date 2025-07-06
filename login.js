439bc3da6a25bf17f61cf5e83f12a398cc547b40
mdSiamTalukder <siamtalukder533@gmail.com>
2025-07-06 09:28:04 +0600

    added simple login handler and prevent default

---
+++ login.js
@@ -0,0 +1,10 @@
+// console.log('button clicking file added');
+document.getElementById('btn-login').addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('login  button click');
+
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber);
+    
+    
+})
