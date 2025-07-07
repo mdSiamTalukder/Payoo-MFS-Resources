24bf01f5c53c54bfd3972cfa2cbeb80ed06e2496
mdSiamTalukder <siamtalukder533@gmail.com>
2025-07-07 11:17:53 +0600

    added partially the add money event handler

---
+++ home.js
@@ -0,0 +1,13 @@
+// add money to the account
+
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+event.preventDefault();
+
+const addMoneyInput = document.getElementById('input-add-money').value;
+console.log(addMoneyInput);
+// get the pin number provided
+const pinNumberInput = document.getElementById('input-pin-number').value;
+console.log(pinNumberInput);
+
+
+})
--- login.js
+++ login.js
@@ -1,10 +1,25 @@
 // console.log('button clicking file added');
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('login  button click');
+    // console.log('login  button click');
 
     const phoneNumber = document.getElementById('phone-number').value;
+    
+
     console.log(phoneNumber);
     
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber);
     
 })
+ 
+
+if(phoneNumber === '5' && pinNumber=== '1234'){
+    console.log('you are logged in');
+    
+     window.location.href = 'home.html'
+}
+
+else{
+    alert('Wrong phone number or Pin');
+}
